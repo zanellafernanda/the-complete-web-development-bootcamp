062f812aacc3b18aa3348549aa85e811e3e8baf3
Fernanda Zanella <zanella@DESKTOP-2ABG267.>
2025-02-20 14:39:08 +0000

    Travel Tracker Part 1
    feat: finalize Travel Tracker Part 1 with constraints

--- backend/postgreSQL/8.3 Travel Tracker/index.js
+++ backend/postgreSQL/8.3 Travel Tracker/index.js
@@ -1,15 +1,39 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const db = new pg.Client({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+})
+
+db.connect();
+
+
 
 const app = express();
-const port = 3000;
+const port = process.env.SERVER_PORT || 3001;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", async (req, res) => {
-  //Write your code here.
+  const result = await db.query("SELECT country_code FROM visited_countries");
+  let countries = [];
+  result.rows.forEach((country) => {
+    countries.push(country.country_code);
+  });
+  console.log(countries);
+  res.render("index.ejs", { countries: countries, total: countries.length });
+  db.end();
 });
+
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
