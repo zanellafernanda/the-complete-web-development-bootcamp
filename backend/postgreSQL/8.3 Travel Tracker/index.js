b2e0cb24d5b235f4ff0247ae3d9ff258e6dd412b
Fernanda Zanella <zanella@DESKTOP-2ABG267.>
2025-03-11 10:25:07 +0000

    Travel Tracker Part 2
    feat: added route /add and added a function checkVisited for reuse

--- backend/postgreSQL/8.3 Travel Tracker/index.js
+++ backend/postgreSQL/8.3 Travel Tracker/index.js
@@ -15,23 +15,39 @@ const db = new pg.Client({
 
 db.connect();
 
-
-
 const app = express();
 const port = process.env.SERVER_PORT || 3001;
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 app.use(express.static("public"));
 
-app.get("/", async (req, res) => {
+async function checkVisited() {
   const result = await db.query("SELECT country_code FROM visited_countries");
   let countries = [];
   result.rows.forEach((country) => {
     countries.push(country.country_code);
   });
-  console.log(countries);
+  return countries;
+}
+
+app.get("/", async (req, res) => {
+  const countries = await checkVisited();
   res.render("index.ejs", { countries: countries, total: countries.length });
-  db.end();
+});
+
+app.post("/add", async (req, res) => {
+  const input = req.body["country"];
+  const result = await db.query("SELECT country_code FROM countries WHERE country_name = $1", [input]);
+
+  if (result.rows.length !== 0) {
+    const data = result.rows[0];
+    const countryCode = data.country_code;
+    
+    await db.query("INSERT INTO visited_countries (country_code) VALUES ($1)", [countryCode]);
+  }
+  res.redirect("/");
 });
 
 
